e159f8cd6a16d00e200efeafea04413fdf3da8c6
MosabAboidrees <mosab_mahala@yahoo.com>
2024-07-24 13:44:53 -0700

    Execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,8 @@
 # This Puppet manifest kills a process named 'killmenow' using pkill.
 
 exec { 'kill_killmenow_process':
-  command     => '/usr/bin/pkill killmenow',
-  path        => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
-  onlyif      => 'pgrep killmenow',
-  logoutput   => true,
+  command   => '/usr/bin/pkill killmenow',
+  path      => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
+  onlyif    => 'pgrep killmenow',
+  logoutput => true,
 }
