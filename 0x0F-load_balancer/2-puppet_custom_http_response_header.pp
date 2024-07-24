c092810c11f79d526768bab8a0cd14f5a434dbd0
MosabAboidrees <mosab_mahala@yahoo.com>
2024-07-24 11:57:19 -0700

    some fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,46 +1,10 @@
-# This Puppet manifest configures Nginx to include a custom HTTP header 'X-Served-By'
-# with the value being the hostname of the server.
+#!/usr/bin/env bash
+# configuration with puppet
 
-node default {
-    # Ensure Nginx is installed
-    package { 'nginx':
-        ensure => installed,
-    }
-
-    # Ensure Nginx service is running and enabled
-    service { 'nginx':
-        ensure     => running,
-        enable     => true,
-        require    => Package['nginx'],
-        subscribe  => File['/etc/nginx/sites-available/default'],
-    }
-
-    # Manage the default site configuration to include the custom header
-    file { '/etc/nginx/sites-available/default':
-        ensure  => file,
-        content => template('nginx/default.erb'),
-        notify  => Service['nginx'],
-    }
-
-    # Ensure the hostname fact is available
-    package { 'facter':
-        ensure => installed,
-    }
-}
-
-# Create a template for the Nginx default site configuration
-file { '/etc/puppetlabs/code/environments/production/modules/nginx/templates/default.erb':
-    ensure  => file,
-    content => '
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    server_name _;
-
-    location / {
-        add_header X-Served-By <%= @hostname %>;
-    }
-}
-',
+exec { 'http header':
+	command  => 'sudo apt-get update -y;
+	sudo apt-get install nginx -y;
+	sudo sed -i "/server_name _/a add_header X-Served-By HOSTNAME;" /etc/nginx/sites-available/default
+	sudo service nginx restart',
+	provider  => shell,
 }
