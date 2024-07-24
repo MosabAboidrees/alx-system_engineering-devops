d392ab4a4da227ce0244462898389e51c5dae9c6
MosabAboidrees <mosab_mahala@yahoo.com>
2024-07-24 13:28:35 -0700

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+# This Puppet manifest creates a file at /tmp/school
+# with specific permissions, owner, group, and content.
+
+file { '/tmp/school':
+  ensure  => 'file',
+  path    => '/tmp/school',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
