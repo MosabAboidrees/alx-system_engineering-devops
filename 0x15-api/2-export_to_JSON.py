afc21c97b0a05bf07026d25b9a735ae59e1172ff
MosabAboidrees <mosab_mahala@yahoo.com>
2024-07-31 16:27:26 -0700

    Export to JSON fix

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """ Gather data from an API """
 import csv
 import json
@@ -113,3 +114,39 @@ def save_tasks_to_csv(tasks_data, users_data, user_id):
     with open(f'{user_id}.csv', mode='w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         for row in user_reco
+        csv_writer.writerow(row)
+
+
+def save_tasks_to_json(tasks_data, users_data, user_id):
+    """
+    Save tasks data to a JSON file
+
+    Arguments:
+        tasks_data (list of dictionaries): JSON format of the tasks info
+        users_data (list of dictionaries): JSON format of the users' info
+        user_id (int): The user ID needed to be fetched
+    """
+    all_records = []
+    user_name = get_username(users_data, user_id)
+    for task in tasks_data:
+        if task["userId"] == user_id and task["title"] is not None:
+            all_records.append({"task": task["title"],
+                                "completed": task["completed"],
+                                "username": user_name})
+    user_tasks = {str(user_id): all_records}
+
+    with open(f"{user_id}.json", 'w', newline='') as json_file:
+        json.dump(user_tasks, json_file)
+
+
+if __name__ == '__main__':
+    response_tasks = urllib.request.urlopen(
+        'https://jsonplaceholder.typicode.com/todos')
+
+    if response_tasks.getcode() == 200:
+        response_users = urllib.request.urlopen(
+            'https://jsonplaceholder.typicode.com/users')
+        if response_users.getcode() == 200:
+            tasks_data = json.loads(response_tasks.read().decode('utf-8'))
+            users_data = json.loads(response_users.read().decode('utf-8'))
+            save_tasks_to_json(tasks_data, users_data, int(sys.argv[1]))
